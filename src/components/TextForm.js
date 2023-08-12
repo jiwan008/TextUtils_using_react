bc060878abfabbd51cea1f5b312b84f813eabe03
Jeevan Kumar Sah <jiwan008@gmail.com>
2023-08-12 11:19:18 +0530

    update

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -50,16 +50,16 @@ export default function TextForm(props) {
         <textarea className="form-control" id="myBox" value={text} rows="8"  onChange={handleOnChange}></textarea>
         </div>
 
-        <button className="btn btn-primary mx-1"  onClick={handleUpClick} >Convert to Uppercase</button> 
-        <button className="btn btn-primary mx-1"  onClick={handleLoClick} >Convert to Lowercase</button> 
-        <button className="btn btn-primary mx-1"  onClick={handleClearClick} >Clear</button> 
-        <button className="btn btn-primary mx-1"  onClick={handleCopy} >Copy Text</button> 
-        <button className="btn btn-primary mx-1"  onClick={handleExtraSpaces} >Remove Extra Spaces </button> 
+        <button className="btn btn-primary mx-1 my-1"  onClick={handleUpClick} >Convert to Uppercase</button> 
+        <button className="btn btn-primary mx-1 my-1"  onClick={handleLoClick} >Convert to Lowercase</button> 
+        <button className="btn btn-primary mx-1 my-1"  onClick={handleClearClick} >Clear</button> 
+        <button className="btn btn-primary mx-1 my-1"  onClick={handleCopy} >Copy Text</button> 
+        <button className="btn btn-primary mx-1 my-1"  onClick={handleExtraSpaces} >Remove Extra Spaces </button> 
     </div>
 
     <div className="container my-3">
     <h2> Your text summary</h2>
-    <p> {text.split(' ').length}, {text.length} characters</p>
+    <p> {text.split(' ').filter((element) => {return element.length !== 0}).length}, {text.length} characters</p>
     <p> {0.008 * text.split(' ').length} Minutes read</p>
     <h2> Preview </h2>
 
